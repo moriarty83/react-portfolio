039807abdfc04025dcbbc0b10cb14efa261c37b6
moriarty83 <moriarty83@gmail.com>
2021-12-06 10:52:40 -0500

    begin navbar dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,35 @@
-import React from "react";
+import {useState} from "react";
+
 
 const Header = (props) =>{
+    const [hidden, setHidden] = useState("hidden")
+
+    const handleHover = () =>{
+        setHidden("")
+    }
+
+    const handleExit = () =>{
+        setHidden("hidden")
+    }
     console.log(props)
     return(
         <nav className={props.mode}>
-            <img src="/icon.png" />
-            <a class={props.mode} href="#about">About</a>
-            <a class={props.mode} href="#skills">Skills</a>
-            <a class={props.mode} href="#projects">Projects</a>
-            <a class={props.mode} href="#resume">Resume</a>
-            <a class={props.mode} href="#contact">Contact</a>
+            <div className="dropdown" onMouseLeave={handleExit}>
+                <img src="/icon.png" onMouseEnter={handleHover}/>
+                <div className={"dropdown-links " + hidden}>
+                    <a class={props.mode} href="#about">About</a>
+                    <a class={props.mode} href="#skills">Skills</a>
+                    <a class={props.mode} href="#projects">Projects</a>
+                    <a class={props.mode} href="#resume">Resume</a>
+                    <a class={props.mode} href="#contact">Contact</a>
+                </div>
+                
+            </div>
+            <a class={"main-link " + props.mode} href="#about">About</a>
+            <a class={"main-link " + props.mode} href="#skills">Skills</a>
+            <a class={"main-link " + props.mode} href="#projects">Projects</a>
+            <a class={"main-link " + props.mode} href="#resume">Resume</a>
+            <a class={"main-link " + props.mode} href="#contact">Contact</a>
         </nav>
     )
 }
