4a43235b8cd37c0fb7d05ab83fd53e11e0fe14fd
moriarty83 <moriarty83@gmail.com>
2022-01-20 14:29:23 -0500

    Detail text styling

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -6,7 +6,8 @@ function Detail (props) {
     return(
         <div class={props.classText}>
             <Zoom>
-            <div class={"inner-border "}>
+            <div class={"inner-border p-1"}>
+                <h3 className="detail-title">{props.title}</h3>
             <p class={"detail-text " + props.mode}>{props.content}</p>
             </div>
             </Zoom>
--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -5,33 +5,31 @@ import Zoom from "react-reveal";
 function About (props) {
     return(
         <Zoom>
-        <div className="section mb-1">
-        
-        
-        <a id="about">
-            <Fade left>
-                <Title title="About"/>
-            </Fade>
-            </a>
-        
-        <div class={"slide " + props.mode}>
-            <div class={"inner-border "+ props.mode}>
-                <div class="flex-col align-start wrap">
-                    <div>
-                        <img class="headshot" src="/images/headshot.jpg" />
-                    </div>
-                    <div>
-                        <div class="content">
-                        I’m a Software Engineer with a background in Business Management and Customer Experience.
-                        </div>
-                        <div class="content">
-                        Thanks to my extensive experience in e-commerce and nonprofit organizations, I approach each project with an empathetic and user-focused perspective. By coupling analytical skills with a love of learning, I am able to solve complex and systematic problems in my pursuit of building unique and empowering experiences that inspire joy and wonder.
+            <div className="section mb-1">
+                <a id="about">
+                    <Fade left>
+                        <Title title="About"/>
+                    </Fade>
+                </a>
+                
+                <div class={"slide " + props.mode}>
+                    <div class={"inner-border "+ props.mode}>
+                        <div class="flex-col align-start wrap">
+                            <div>
+                                <img class="headshot" src="/images/headshot.jpg" />
+                            </div>
+                            <div>
+                                <div class="content">
+                                I’m a Software Engineer with a background in Business Management and Customer Experience.
+                                </div>
+                                <div class="content">
+                                Thanks to my extensive experience in e-commerce and nonprofit organizations, I approach each project with an empathetic and user-focused perspective. By coupling analytical skills with a love of learning, I am able to solve complex and systematic problems in my pursuit of building unique and empowering experiences that inspire joy and wonder.
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        </div>
         </Zoom>
     )
 }
