a9a0fa61802ba2fc6903bf50cb0e08d5e88a3db9
moriarty83 <moriarty83@gmail.com>
2022-01-23 14:22:03 -0500

    aavso about

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -26,7 +26,10 @@ function About (props) {
                                 Thanks to my extensive experience in e-commerce and nonprofit organizations, I approach each project with an empathetic and user-focused perspective. By coupling analytical skills with a love of learning, I am able to solve complex and systematic problems in my pursuit of building unique and empowering experiences that inspire joy and wonder.
                                 </div>
                                 <div class="content">
-                                As a Cellist, I have been privileged to play with the Cambridge Symphony Orchestra and the Reading Symphony Orchestra. It has been an honor to be a member of these organizations; made up of people who prize teamwork, creativity, compassion and inclusion. 
+                                I have been privileged to play cello with the Cambridge Symphony Orchestra and the Reading Symphony Orchestra. It has been an honor to be a member of these organizations; made up of people who prize teamwork, creativity, compassion and inclusion. 
+                                </div>
+                                <div class="content">
+                                As a member of the American Association of Variable Star Observers I have strived to make meaningful contributions to this citizen science organization; helping to further our shared knowledge of the stars and cosmos. 
                                 </div>
                             </div>
                         </div>
