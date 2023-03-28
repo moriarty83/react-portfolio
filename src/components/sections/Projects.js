2712ef5e971c36f38aa7bbbd90bc4948f411cff2
moriarty83 <moriarty83@gmail.com>
2023-03-28 10:09:27 -0400

    minor update to skills and resume

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,110 +1,220 @@
-import Title from "../Title"
-import Project from "../Project"
+import Title from "../Title";
+import Project from "../Project";
 import { Fade } from "@stahl.luke/react-reveal";
 
-
-
 function Projects(props) {
-    return (
-        <div className="section">
-            <a id="projects">
-                <Fade left>
-                    <Title title="Projects" />
-                </Fade></a>
-            <div className={"slide project " + props.mode}>
-                <div className={"inner-border " + props.mode}>
-                    <div className="content table">
-                        <Project
-                            project="civic"
-                            title="Civic Voter Companion"
-                            content={<div className="pl-1 pb-1 pr-1">
-                                <p className="detail-text">Civic engagement is of great interest and importance to me. This drove me to create the Civic Voter Companion to enable people to easily learn about their elected officials and find out how to vote and become involved in government.</p><br />
-                                <p className="detail-text">Using this app, can not only access offical's websites and social media profiles but link directly to their state's voter registration and election information.</p><br />
-                                <p className="detail-text">When a user's address matches that of an upcoming election, they can also see what offices are up for election and/or questions will be on the ballot.</p><br />
-                                <a className="detail-text" target="_blank" href="https://apps.apple.com/us/app/civic-voting-companion/id1606433760">View on the AppStore</a>
-
-                            </div>}
-                            technologies="Swift | Firebase Auth | Firestore DB | iOS"
-                        />
-
-                        <Project
-                            project="outta-pocket"
-                            title="Outta Pocket"
-                            content={<div className="pl-1 pb-1 pr-1">
-                                <p className="detail-text">Outta Pocket is the product of working closely with a team of UX Designers for the General Assembly February 2022 Outcomes Hackathon.</p><br />
-                                <p className="detail-text">An original content platform, Outta Pocket aims to provide an informative and safe place for people to learn how to take control of their financial future. </p><br />
-                                <p className="detail-text">My responsibilities included, Version Control, reconciling merge conflcits and developing major aspects of the front-end functionality, it was a pleasure to deliver a great application in fewer than 72 hours.</p>
-                                <a className="detail-text" target="_blank" href="https://outta-pocket.netlify.app/">View Site</a>
-
-                            </div>}
-                            technologies="React | Express | MongoDB | Node.JS | Vimeo"
-                        />
-
-                        <Project
-                            project="mykitchen"
-                            title="MyKitchen"
-                            content={<div className="pl-1 pb-1 pr-1">
-                                <p className="detail-text">Too many times have I found a recipe online that looked like the perfect use for that piece of salmon I have in the fridge, only to find the only ingredient I had to make the dish was that piece of fish.</p><br />
-                                <p className="detail-text">MyKitchen is a Pantry & Recipe Application to address this issue. Powered by the Edemam Recipe and Food API, users not only search for recipes, but are able to create a personal pantry of items they regularly have on hand. </p><br />
-                                <p className="detail-text">This makes recipe searches smarter; by showing users what they already have the pantry or refrigerator.</p>
-                                <a className="detail-text" target="_blank" href="https://my-kitchen-site.netlify.app/">View Site</a>
-
-                            </div>}
-                            technologies="React | Ruby | Rails | Tailwind CSS | PostgreSQL | REST API"
-                        />
-
-                        <Project
-                            project="cribbage"
-                            title="Cribbage a Classy Card Game"
-                            content={<div className="pl-1 pb-1 pr-1">
-                                <p className="detail-text">A single player Cribbage game on the App Store and Google Play.</p><br />
-                                <p className="detail-text">It features a full tutorial, pleasant graphics, 3 levels of difficulty, and an adorable AI Turtle as your opponent.</p><br />
-                                <p className="detail-text">This was my first coding project and something I'm still very proud of both by the final product and the amount of hard work, learning and discovery that went into it.</p><br />
-                                <a className="detail-text detail-link" target="_blank" href="https://apps.apple.com/us/app/cribbage-a-classy-card-game/id1521767830">View on the AppStore</a>
-
-                            </div>}
-                            technologies="C# | Unity | GooglePlay | AppStore"
-                        />
-
-                        <Project
-                            project="albatross"
-                            title="Albatross"
-                            content={<div className="pl-1 pb-1 pr-1">
-                                <p className="detail-text">A personal golf scorecard for Traditional, Putt Putt, and Disc Golfers.</p><br />
-                                <p className="detail-text">Users create a library of courses, can log games and track their performance over time.</p><br />
-                                <p className="detail-text">Courses can be shared with the Albatross community and contributed to the larger library of courses.</p><br />
-                                <a className="detail-text" target="_blank" href="https://albatross4golf.herokuapp.com/">View Site</a>
-                            </div>}
-                            technologies="JavaScript | MongoDB | Express | Auth0"
-                        />
-
-                        <Project
-                            project="rp_rover"
-                            title="Red Planet Rover Images"
-                            content={<div className="pl-1 pb-1 pr-1">
-                                <p className="detail-text">Made as my final project for HarvardX's Intro to Computer Science course, Red Planet Rover Images uses Nasa's Mars Rover API to find images from the Spirit, Curiosity, & Opportunity Rovers.</p><br />
-                                <p className="detail-text">Complete with user registration and authentication users are able to create their own library of favorite images from all three rovers and suggestions for interesting dates to look at.</p><br />
-                                <a className="detail-text" target="_blank" href="https://github.com/moriarty83/red-planet-rover">View on GitHub</a>
-                            </div>}
-                            description="Made as my final project for HarvardX's Intro to Computer Science course, Red Planet Rover Images uses Nasa's Mars Rover API to let users lookup and create a gallery of images from the Spirit, Curiosity, & Opportunity Rovers."
-                            technologies="Python | Flask | Jquery | HTML | CSS | NASA Mars Rover API"
-                        />
-
-                        <Project
-                            project="sol_trivia"
-                            title="Sol System Trivia"
-                            content={<div className="pl-1 pb-1 pr-1">
-                                <p className="detail-text">Sol System Trivia is a simple, two players/one device trivia game about the solar system.</p><br />
-                                <p className="detail-text">Questions are generated through a headless API hosted on Contentful and the entire game exists on a single page, played without reloads.</p><br />
-                                <a className="detail-text" target="_blank" href="https://moriarty83.github.io/sol-system-trivia/">View Site</a>
-                            </div>}
-                            technologies="TypeScript | Jquery | HTML | CSS | Headless API"
-                        />
-                    </div>
+  return (
+    <div className="section">
+      <a id="projects">
+        <Fade left>
+          <Title title="Projects" />
+        </Fade>
+      </a>
+      <div className={"slide project " + props.mode}>
+        <div className={"inner-border " + props.mode}>
+          <div className="content table">
+            <Project
+              project="civic"
+              title="Civic Voter Companion"
+              content={
+                <div className="pl-1 pb-1 pr-1">
+                  <p className="detail-text">
+                    Civic engagement is of great interest and importance to me.
+                    This drove me to create the Civic Voter Companion to enable
+                    people to easily learn about their elected officials and
+                    find out how to vote and become involved in government.
+                  </p>
+                  <br />
+                  <p className="detail-text">
+                    Using this app, can not only access offical's websites and
+                    social media profiles but link directly to their state's
+                    voter registration and election information.
+                  </p>
+                  <br />
+                  <p className="detail-text">
+                    When a user's address matches that of an upcoming election,
+                    they can also see what offices are up for election and/or
+                    questions will be on the ballot.
+                  </p>
+                  <br />
+                  <a
+                    className="detail-text"
+                    target="_blank"
+                    href="https://apps.apple.com/us/app/civic-voting-companion/id1606433760"
+                  >
+                    View on the AppStore
+                  </a>
                 </div>
-            </div>
+              }
+              technologies="Swift | Firebase Auth | Firestore DB | iOS"
+            />
+
+            <Project
+              project="outta-pocket"
+              title="Outta Pocket"
+              content={
+                <div className="pl-1 pb-1 pr-1">
+                  <p className="detail-text">
+                    Outta Pocket is the product of working closely with a team
+                    of UX Designers for the General Assembly February 2022
+                    Outcomes Hackathon.
+                  </p>
+                  <br />
+                  <p className="detail-text">
+                    An original content platform, Outta Pocket aims to provide
+                    an informative and safe place for people to learn how to
+                    take control of their financial future.{" "}
+                  </p>
+                  <br />
+                  <p className="detail-text">
+                    My responsibilities included, Version Control, reconciling
+                    merge conflcits and developing major aspects of the
+                    front-end functionality, it was a pleasure to deliver a
+                    great application in fewer than 72 hours.
+                  </p>
+                  <a
+                    className="detail-text"
+                    target="_blank"
+                    href="https://outta-pocket.netlify.app/"
+                  >
+                    View Site
+                  </a>
+                </div>
+              }
+              technologies="React | Express | MongoDB | Node.JS | Vimeo"
+            />
+
+            <Project
+              project="mykitchen"
+              title="MyKitchen"
+              content={
+                <div className="pl-1 pb-1 pr-1">
+                  <p className="detail-text">
+                    Too many times have I found a recipe online that looked like
+                    the perfect use for that piece of salmon I have in the
+                    fridge, only to find the only ingredient I had to make the
+                    dish was that piece of fish.
+                  </p>
+                  <br />
+                  <p className="detail-text">
+                    MyKitchen is a Pantry & Recipe Application to address this
+                    issue. Powered by the Edemam Recipe and Food API, users not
+                    only search for recipes, but are able to create a personal
+                    pantry of items they regularly have on hand.{" "}
+                  </p>
+                  <br />
+                  <p className="detail-text">
+                    This makes recipe searches smarter; by showing users what
+                    they already have the pantry or refrigerator.
+                  </p>
+                  <a
+                    className="detail-text"
+                    target="_blank"
+                    href="https://my-kitchen-site.netlify.app/"
+                  >
+                    View Site
+                  </a>
+                </div>
+              }
+              technologies="React | Ruby | Rails | Tailwind CSS | PostgreSQL | REST API"
+            />
+
+            <Project
+              project="cribbage"
+              title="Cribbage a Classy Card Game"
+              content={
+                <div className="pl-1 pb-1 pr-1">
+                  <p className="detail-text">
+                    A single player Cribbage game on the App Store and Google
+                    Play.
+                  </p>
+                  <br />
+                  <p className="detail-text">
+                    It features a full tutorial, pleasant graphics, 3 levels of
+                    difficulty, and an adorable AI Turtle as your opponent.
+                  </p>
+                  <br />
+                  <p className="detail-text">
+                    This was my first coding project and something I'm still
+                    very proud of both by the final product and the amount of
+                    hard work, learning and discovery that went into it.
+                  </p>
+                  <br />
+                  <a
+                    className="detail-text detail-link"
+                    target="_blank"
+                    href="https://apps.apple.com/us/app/cribbage-a-classy-card-game/id1521767830"
+                  >
+                    View on the AppStore
+                  </a>
+                </div>
+              }
+              technologies="C# | Unity | GooglePlay | AppStore"
+            />
+            <Project
+              project="rp_rover"
+              title="Red Planet Rover Images"
+              content={
+                <div className="pl-1 pb-1 pr-1">
+                  <p className="detail-text">
+                    Made as my final project for HarvardX's Intro to Computer
+                    Science course, Red Planet Rover Images uses Nasa's Mars
+                    Rover API to find images from the Spirit, Curiosity, &
+                    Opportunity Rovers.
+                  </p>
+                  <br />
+                  <p className="detail-text">
+                    Complete with user registration and authentication users are
+                    able to create their own library of favorite images from all
+                    three rovers and suggestions for interesting dates to look
+                    at.
+                  </p>
+                  <br />
+                  <a
+                    className="detail-text"
+                    target="_blank"
+                    href="https://github.com/moriarty83/red-planet-rover"
+                  >
+                    View on GitHub
+                  </a>
+                </div>
+              }
+              description="Made as my final project for HarvardX's Intro to Computer Science course, Red Planet Rover Images uses Nasa's Mars Rover API to let users lookup and create a gallery of images from the Spirit, Curiosity, & Opportunity Rovers."
+              technologies="Python | Flask | Jquery | HTML | CSS | NASA Mars Rover API"
+            />
+
+            <Project
+              project="sol_trivia"
+              title="Sol System Trivia"
+              content={
+                <div className="pl-1 pb-1 pr-1">
+                  <p className="detail-text">
+                    Sol System Trivia is a simple, two players/one device trivia
+                    game about the solar system.
+                  </p>
+                  <br />
+                  <p className="detail-text">
+                    Questions are generated through a headless API hosted on
+                    Contentful and the entire game exists on a single page,
+                    played without reloads.
+                  </p>
+                  <br />
+                  <a
+                    className="detail-text"
+                    target="_blank"
+                    href="https://moriarty83.github.io/sol-system-trivia/"
+                  >
+                    View Site
+                  </a>
+                </div>
+              }
+              technologies="TypeScript | Jquery | HTML | CSS | Headless API"
+            />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -1,55 +1,56 @@
-
-import Title from "../Title"
-import Detail from "../Detail"
-import Skill from "../Skill"
+import Title from "../Title";
+import Detail from "../Detail";
+import Skill from "../Skill";
 
 import { Fade } from "@stahl.luke/react-reveal";
 
 function Skills(props) {
-
-    return (
-        <div className="section">
-            <a id="skills">
-                <Fade left>
-                    <Title title="Skills" />
-                </Fade></a>
-            <div className={"slide " + props.mode}>
-                <div className={"inner-border " + props.mode}>
-                    <div className="content table">
-                        <Skill title="Amazon Web Services" skill="aws" content="" />
-                        <Skill title="C#" skill="csharp" content="My first love in programming, I've used C# extensively for Game Development" />
-                        <Skill title="CSS" skill="css3" content="I'm a huge fan of pure CSS methods of making great user experiences with things like animations, transitions, sticky scrolling and more" />
-                        <Skill title="Django" skill="django" content="The fact that it's named for Django Reinhardt is just one of reasons to love this framework. From blogs to income/expense applications, I've loved Django's features, out-of-the-box usability and scalability" />
-                        <Skill title="Express.js" skill="expressjs" content="" />
-                        <Skill title="Firebase" skill="firebase" content="" />
-                        <Skill title="Flask" skill="flask" content="I built my first full stack application, Red Planet Rover Images, with Flask as well as an application dashboard for Teddy the Dog Apparel on Flask. It isn't my favorite framework but it's polite and unassuming which I respect." />
-                        <Skill title="Git" skill="git" content="" />
-                        <Skill title="GitHub" skill="github" content="" />
-                        <Skill title="Heroku" skill="heroku" content="" />
-                        <Skill title="HTML 5" skill="html5" content="Having built web applications with everything with just HTML & JavaScript to JSX, React, & Ruby on Rails, good ol' HTML is like an security blanket to me." />
-                        <Skill title="JavaScript" skill="javascript" content="" />
-                        <Skill title="MERN Stack" skill="mern" content="" />
-                        <Skill title="Mongo DB" skill="mongodb" content="" />
-                        <Skill title="MySQL" skill="mysql" content="I've used MySQL extensively to track unfulfilled orders for Teddy the Dog Apparel and for numerous Ruby on Rails applications" />
-                        <Skill title="Node.js" skill="nodejs" content="" />
-                        <Skill title="Photoshop" skill="photoshop" content="" />
-                        <Skill title="PostgreSQL" skill="postgresql" content="" />
-                        <Skill title="Python" skill="python" content="" />
-                        <Skill title="Rails" skill="rails" content="" />
-                        <Skill title="React" skill="react" content="" />
-                        <Skill title="Ruby" skill="ruby" content="" />
-                        <Skill title="SASS" skill="sass" content="There really is nothing like being able to nest CSS command and mix in some mixins with SASS." />
-                        <Skill title="Swift" skill="swift" content="" />
-                        <Skill title="TypeScript" skill="typescript" content="" />
-                        <Skill title="Unity" skill="unity" content="Unity was how I entered the world of programming. I built my first project, Cribbage - A Classy Card Game, and I owe so much to the peole who create this wondeful product and the community that uses, supports, and champions it." />
-                        <Skill title="Visual Studio" skill="visualstudio" content="Unity was how I entered the world of programming. I built my first project, Cribbage - A Classy Card Game, and I owe so much to the peole who create this wondeful product and the community that uses, supports, and champions it." />
-                        <Skill title="VS Code" skill="vscode" content="" />
-                        <Skill title="X Code" skill="xcode" content="" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="section">
+      <a id="skills">
+        <Fade left>
+          <Title title="Skills" />
+        </Fade>
+      </a>
+      <div className={"slide " + props.mode}>
+        <div className={"inner-border " + props.mode}>
+          <div className="content table">
+            <Skill title="Amazon Web Services" skill="aws" content="" />
+            <Skill title="C#" skill="csharp" />
+            <Skill title="CSS" skill="css3" />
+            <Skill title="Django" skill="django" />
+            <Skill title="Express.js" skill="expressjs" />
+            <Skill title="Firebase" skill="firebase" />
+            <Skill title="Flask" skill="flask" />
+            <Skill title="Git" skill="git" />
+            <Skill title="GitHub" skill="github" />
+            <Skill title="Heroku" skill="heroku" />
+            <Skill title="HTML 5" skill="html5" />
+            <Skill title="JavaScript" skill="javascript" />
+            <Skill title="MERN Stack" skill="mern" />
+            <Skill title="Mongo DB" skill="mongodb" />
+            <Skill title="MySQL" skill="mysql" />
+            <Skill title="Node.js" skill="nodejs" />
+            <Skill title="Photoshop" skill="photoshop" />
+            <Skill title="PostgreSQL" skill="postgresql" />
+            <Skill title="Python" skill="python" />
+            <Skill title="Rails" skill="rails" />
+            <Skill title="React" skill="react" />
+            <Skill title="Ruby" skill="ruby" />
+            <Skill title="SASS" skill="sass" />
+            <Skill title="Shopify" skill="shopify" />
+            <Skill title="Swift" skill="swift" />
+            <Skill title="TypeScript" skill="typescript" />
+            <Skill title="Unity" skill="unity" />
+            <Skill title="Visual Studio" skill="visualstudio" />
+            <Skill title="VS Code" skill="vscode" />
+            <Skill title="Vue.js" skill="vuejs" />
+            <Skill title="X Code" skill="xcode" />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
